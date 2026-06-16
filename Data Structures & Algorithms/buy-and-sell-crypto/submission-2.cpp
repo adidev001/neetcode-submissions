499ecda269f0d77431f9969a6ba883ed7726ab7e
Devansh Rai <adidevrai001@gmail.com>
2026-06-16 17:26:03 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxp=0;
+        for(int b=0;b<n-1;b++){
+            int s=b+1;
+            if(arr[b]<=arr[s]&&j<n){
+                maxp=max(max,prices[s]-prices[b]);
+                s++;
+            }else{
+                s++;
+            }
+        }
+
+        return maxp;
+    }
+};
