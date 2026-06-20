481bdb50984bfd861bb95246b3f50e5b55f85a10
Devansh Rai <adidevrai001@gmail.com>
2026-06-20 19:00:39 +0530

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=0;
+        int r=x/2;
+        while(l<=r){
+            int m=l+(r-1)/2;
+            if(m*m == x)return m;
+            else if(m*m>x){
+                r=m-1;
+
+            }else{
+                l=m+1;
+            }
+        }return m;
+    }
+};
