c7471924543bb7199151c3715ebce8d91378e2ff
Devansh Rai <adidevrai001@gmail.com>
2026-07-24 16:43:07 +0530

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char,int>map;
+        for(auto it : s){
+            map[it]++; 
+        }
+        priority_queue<pair<int,char>>pq;
+        for(auto& a:map){
+            pq.push({a.second,a.first});
+        }
+
+        string res="";
+
+        pair<char,item>waiting={0,'#'};
+
+        while(!pq.empty()){
+            auto current= pq.top();
+            pq.pop();
+
+            res+=current.second;
+
+
+            if(waiting.firs>0){
+                pq.push(waiting);
+            }
+
+            waiting={current.first-1,current.second};
+
+
+
+        }
+
+        retrun (res.size()==s.size())?res:"";
+    }
+};
