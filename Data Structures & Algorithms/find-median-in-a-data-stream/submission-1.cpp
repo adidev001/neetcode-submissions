dea2d0e53e89f000ca9a2b8a6191efada49ed4fa
Devansh Rai <adidevrai001@gmail.com>
2026-07-25 19:38:24 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,22 @@
+class MedianFinder {
+    vector<int> arr;
+    int sum =0;
+    int count=0;
+public:
+    MedianFinder() {
+        
+
+
+    }
+    
+    void addNum(int num) {
+        arr.push_back(num);
+        sum+=num;
+        count++;
+
+    }
+    
+    double findMedian() {
+        return double(sum/count);
+    }
+};
