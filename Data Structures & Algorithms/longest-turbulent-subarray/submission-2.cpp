f7e240a2a79ff90e1e6d028ef67f550a0cebb4d1
Devansh Rai <adidevrai001@gmail.com>
2026-08-02 15:30:41 +0530

    Add: longest-turbulent-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int l=0;
+        int r=1;
+        int res=1;
+        int n=arr.size();
+        string prev="";
+
+        while(r<n){
+            if(arr[r-1]>arr[r] && prev!=">"){
+                res =max(res,r-l+1);
+                r++;
+                prev=">";
+            }
+            else if(arr[r-1]<arr[r] && prev!="<"){
+                res =max(res,r-l+1);
+                r++;
+                prev="<";
+            }
+            else {
+                
+                if(arr[r-1]==arr[r]){
+                    l=r;
+                    r++;
+                    prev="";
+                }else{
+                    l=r-1;
+                    prev="";
+                }
+            }
+        }return res;
+
+    }
+};
