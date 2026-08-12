7141f237566e9d3950a34f5a4e1bf75158951548
Devansh Rai <adidevrai001@gmail.com>
2026-08-13 00:10:57 +0530

    Add: jump-game-vii - submission-1

---
+++ Data Structures & Algorithms/jump-game-vii/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n= s.length()-1;
+        queue <int>q;
+        if (s.back() == '1') return false;
+        q.push(0);
+        int farthest =0;
+        while(!q.empty()){
+            int curr=q.top();
+            q.pop();
+
+            if(q.top()==n-1){
+                return true;
+            }
+
+            int left= max(curr+minJump,farthest);
+            int right=min(curr+maxJump,n-1);
+
+            for(int i=left;i<=right;i++){
+                if(s[i]=='0'){
+                    q.push(i);
+                }
+            }
+
+            farthest=max(farthest ,curr+maxJump);
+
+
+        }return false;
+
+    }
+};
