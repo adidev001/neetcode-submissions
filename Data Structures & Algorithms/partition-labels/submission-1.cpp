841480b1ba44e1e4d347bbb17da3236bee55a774
Devansh Rai <adidevrai001@gmail.com>
2026-08-18 00:37:29 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        map<char , int > map;
+
+        for (int i = 0; i < s.length(); i++) {
+             map[s[i]] = i;
+            }
+        vector<int>res;
+        for(int i=0;i<string.length();i++){
+            int start =0;
+            int end=0;
+            end= max(end,map[s[i]]);
+
+            if(i==end){
+                res.push_back(end-start+1);
+                start=i+1;
+                end=i+1;
+            }
+        }
+        return res;
+    }
+};
