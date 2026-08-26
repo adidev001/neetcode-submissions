b03475692d62154ffb789e66f68ddbde53e0a00e
Devansh Rai <adidevrai001@gmail.com>
2026-08-26 13:26:27 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+   void bt(int index,vector<int>& nums,vector<vector<int>>res,vector<int>sub){
+        if(index==nums.size()){
+            res.push_back(sub);
+            return res;
+        }
+
+        sub.push_back(nums[index]);
+        bt(index+1,nums,res,sub);
+
+        sub.pop();
+        bt(index+1,nums,res,sub);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        vector<int>sub;
+        bt(0,nums,res,sub);
+        return res;
+    }
+};
