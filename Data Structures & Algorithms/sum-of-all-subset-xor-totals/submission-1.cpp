ef8cc153bced27561e51dabd865fa8594736a70f
Devansh Rai <adidevrai001@gmail.com>
2026-08-26 11:52:49 +0530

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.cpp
@@ -0,0 +1,15 @@
+int backtrack(vector<int>& nums, int index, int currentXOR) {
+    if (index == nums.size()) {
+        return currentXOR;
+    }
+    
+    // Sum of excluding and including current element
+    int exclude = backtrack(nums, index + 1, currentXOR);
+    int include = backtrack(nums, index + 1, currentXOR ^ nums[index]);
+    
+    return exclude + include;
+}
+
+int subsetXORSum(vector<int>& nums) {
+    return backtrack(nums, 0, 0);
+}
