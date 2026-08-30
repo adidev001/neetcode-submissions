0cb9763524a1df689aeeadf05fb3f771edd94fa8
Devansh Rai <adidevrai001@gmail.com>
2026-08-30 11:54:24 +0530

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void bt(vector<int>& nums, int target,int sum,vector<vector<int>>&res,vector<int>&arr,int index){
+
+        if(sum==target)res.push_back(arr);
+        if(index==nums.size())return;
+        if(sum>target)return;
+
+
+        bt(nums,index,sum+=arr[index]);
+        arr.push_back(nums[index]);
+
+        bt(nums,index+1,sum);
+        arr.pop_back();
+
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>>res;
+        vector<int>arr;
+        int index;
+        bt(nums,target,0,res,arr,0);
+        return res;
+    }
+};
