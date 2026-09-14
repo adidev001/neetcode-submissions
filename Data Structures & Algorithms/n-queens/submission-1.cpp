6327ca4ea64cb67fa32045c383074de678bce685
Devansh Rai <adidevrai001@gmail.com>
2026-09-14 23:13:34 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+   public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+
+        vector<string> board(n, string(n, '.'));
+        solve(board, res, 0);
+        return res;
+    }
+
+    void solve(vector<string> &board,vector<vector<string>> &res,int row){
+        
+        if(row==board.size()){
+            res.push_back(board);
+            return ;
+        }
+        for (int col=0;col<board.size();col++){
+            if(issafe(row,col,board)){
+                board[row][col]='Q';
+                solve(board,res,row+1);
+                board[row][col]='.';
+            }
+
+
+        }
+    }
+
+    bool issafe(int row,int c,vector<string>& board){
+        for(int i =r-1 ; i>=0;i-- ){
+            if(board[r][c]=='Q')return false;
+
+        }
+
+        for(int i=r-1,int j=c-1;i>=0,j>=0;i--,j--){
+            if(board[r][c]=='Q')return false;
+        }
+
+        for(int i=r-1,int j=c+1;i>=0,j<board.size();i--,j++){
+            if(board[r][c]=='Q')return false;
+        }
+        return true;
+    }
+};
